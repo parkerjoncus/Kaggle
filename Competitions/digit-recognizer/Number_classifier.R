7f928ff0ac85c7f280c8d1633fbfcc0013d4a8dc
Parker Joncus <parkerj@me.com>
2020-09-11 12:28:54 -0500

    Add Neural Net to code
    Used H2O to create nerual net predictions.

--- Competitions/digit-recognizer/Number_classifier.R
+++ Competitions/digit-recognizer/Number_classifier.R
@@ -2,6 +2,8 @@ library(readr)
 library(caret)
 library(tensorflow)
 library(randomForest)
+library(h2o)
+library(generics)
 
 trainPct <- .8
 testPct <- 1 - trainPct
@@ -40,7 +42,7 @@ rf.predict <- predict(rf.model, Test)
 print(rf.cm <- confusionMatrix(rf.predict, Test$label))
 
 dataTest <- cbind(Prediction = predict(rf.model, dataTest), dataTest)
-dataTest$Prediction <- as.double(dataTest$Prediction)-1
+dataTest$Prediction <- as.numeric(dataTest$Prediction)-1
 
 plotTest <- function(images){
   op <- par(no.readonly=TRUE)
@@ -64,4 +66,27 @@ colnames(submission1)<-c("ImageId","Label")
 submission1$ImageId<-as.integer(submission1$ImageId)
 write.csv(submission1, file = "~/Documents/GitHub/Kaggle/Competitions/digit-recognizer/submissions/RFsubmission1.csv", row.names = FALSE)
 
+#Deep Nural Network
+h2o.init()
+h2o.train <- as.h2o(Train)
+h2o.test <- as.h2o(Test)
+h2o.model <- h2o.deeplearning(x = setdiff(names(Train), c("label")),
+                              y = "label",
+                              training_frame = h2o.train,
+                              standardize = TRUE,         # standardize data
+                              hidden = c(100, 100),       # 2 layers of 00 nodes each
+                              rate = 0.05,                # learning rate
+                              epochs = 10,                # iterations/runs over data
+                              seed = 1234                 # reproducability seed
+)
+h2o.predictions <- as.data.frame(h2o.predict(h2o.model, h2o.test))
+print(h2o.cm <- confusionMatrix(h2o.predictions$predict, Test$label))
 
+temp <- as.data.frame(h2o.predict(h2o.model, as.h2o(dataTest)))
+dataTest$Prediction <- as.numeric(temp$predict)-1
+plotTest(1:100)
+
+submission2<- data.frame(row.names(dataTest),dataTest$Prediction)
+colnames(submission2)<-c("ImageId","Label")
+submission2$ImageId<-as.integer(submission2$ImageId)
+write.csv(submission2, file = "~/Documents/GitHub/Kaggle/Competitions/digit-recognizer/submissions/NNsubmission1.csv", row.names = FALSE)
