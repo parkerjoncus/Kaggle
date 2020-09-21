b0efdf1481db99d81cb2e5eaa5de4fcfac7c67b4
Parker Joncus <parkerj@me.com>
2020-09-21 13:58:34 -0500

    add function to lower resolution manually
    function now correctly lowers the resolution by powers of 2

--- Datasets/Lego/lego.R
+++ Datasets/Lego/lego.R
@@ -1,6 +1,7 @@
 library(caret)
 library(jpeg)
 library(readr)
+library(raster)
 setwd("~/Documents/GitHub/Kaggle/Datasets/Lego")
 index <- read_csv("index.csv")
 metadata <- read_csv("metadata.csv")
@@ -19,14 +20,26 @@ JPEGToPixels <- function(imagePath){
   return(t(as.vector(t(image))))
 }
 
-CreateLowerResolution <- function(image){
-  image<-as.data.frame(image)
-  newImage <- as.data.frame(matrix(nrow = 512, ncol = 256))
-  for (i in 1:256){
-    cbind(newImage,rowMeans(image[,i:(i+5)]))
+CreateLowerResolution <- function(image,divisor){
+  totalsize<-512/divisor
+  return<-array(0,dim=c(totalsize,totalsize,3))
+  for (i in 1:3){  
+    middleImage <- matrix(nrow = 512, ncol = totalsize)
+    for (j in 1:totalsize){
+      index1<-(j*divisor)-(divisor-1)
+      middleImage[,j]=rowMeans(image[,index1:(index1+3),i])
+    }
+    for (k in 1:totalsize){
+      index2<-(k*divisor)-(divisor-1)
+      return[k,,i]=colMeans(middleImage[index2:(index2+3),])
+    }
   }
-  return(newImage)
+  return(return)
 }
+harrypotter1 <- readJPEG('~/Documents/GitHub/Kaggle/Datasets/Lego/harry-potter/0001/001.jpg')
+newharrypotter<-CreateLowerResolution(harrypotter1,4)
+plot(1:2,1:2, type='n')
+rasterImage(newharrypotter, 1, 1, 2, 2)
 
 for (i in 1:length(train$class_id)){
   print(i)
@@ -38,6 +51,6 @@ View(harrypotter1)
 harrypotter2 <- readJPEG('~/Documents/GitHub/Kaggle/Datasets/Lego/marvel/0002/002.jpg')
 View(harrypotter2)
 plot(1:2,1:2, type='n')
-rasterImage(harrypotter2, 1, 1, 2, 2)
+rasterImage(harrypotter1, 1, 1, 2, 2)
 
 
