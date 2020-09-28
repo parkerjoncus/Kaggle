a93a75554001305f899dbabf2c03e6ac6cf766a7
Parker Joncus <parkerj@me.com>
2020-09-28 13:11:23 -0500

    add h2o neural network

--- Datasets/Lego/lego.R
+++ Datasets/Lego/lego.R
@@ -1,7 +1,9 @@
 library(caret)
 library(jpeg)
+library(h2o)
 library(readr)
 library(raster)
+library(randomForest)
 setwd("~/Documents/GitHub/Kaggle/Datasets/Lego")
 index <- read_csv("index.csv")
 metadata <- read_csv("metadata.csv")
@@ -10,6 +12,7 @@ View(metadata)
 
 metadata$X1 <- NULL
 index$X1 <- NULL
+index$class_id<-as.factor(index$class_id)
 
 train<- index[index$`train-valid`=="train",]
 test<- index[index$`train-valid`=="valid",]
@@ -38,6 +41,7 @@ plot(1:2,1:2, type='n')
 rasterImage(newharrypotter, 1, 1, 2, 2)
 
 Train<-as.data.frame(matrix(0,ncol = numberofRows^2*3,nrow = 144))
+Test<-as.data.frame(matrix(0,ncol = numberofRows^2*3,nrow = 132))
 
 for (i in 1:length(train$class_id)){
   image<-readJPEG(train$path[i])
@@ -46,5 +50,32 @@ for (i in 1:length(train$class_id)){
 }
 Train<-cbind(label=train$class_id,Train)
 
+for (i in 1:length(test$class_id)){
+  image<-readJPEG(test$path[i])
+  image<-CreateLowerResolution(image,numberofDivision)
+  Test[i,]<-t(as.vector(image))
+}
+Test<-cbind(label=test$class_id,Test)
+
+#Random Forrest
+rf.model <- randomForest(label ~ ., data = Train, ntree = 100, nodesize = 50)
+rf.predict <- predict(rf.model, Test)
+print(rf.cm <- confusionMatrix(rf.predict, Test$label))
+
+#h2o
+h2o.init()
+h2o.train <- as.h2o(Train)
+h2o.test <- as.h2o(Test)
+h2o.model <- h2o.deeplearning(x = setdiff(names(Train), c("label")),
+                              y = "label",
+                              training_frame = h2o.train,
+                              standardize = TRUE,         # standardize data
+                              hidden = c(100, 100),       # 2 layers of 00 nodes each
+                              rate = 0.05,                # learning rate
+                              epochs = 100,                # iterations/runs over data
+                              seed = 1234                 # reproducability seed
+)
+h2o.predictions <- as.data.frame(h2o.predict(h2o.model, h2o.test))
+print(h2o.cm <- confusionMatrix(h2o.predictions$predict, Test$label))
 
 
